d7e0b2433f6797d4a9675034e858cb425b7423d1
bulian1311 <bulian1311@gmail.com>
2020-10-05 16:02:50 +0300

    created manifest files for skaffold a skaffold autn and ingreese

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hey!");
+});
+
 app.listen(4000, () => {
   console.log("Auth listening on port 4000...");
 });
