828ecb8fb5b5a80031a41a19b7058fc6103813c6
bulian1311 <bulian1311@gmail.com>
2020-11-07 17:44:36 +0300

    Starting nessage in orders service.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,8 @@ import { ProductUpdatedListener } from "./events/listeners/product-updated.liste
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete.listener";
 
 const start = async () => {
+  console.log("Starting...");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined.");
   }
